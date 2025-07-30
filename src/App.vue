f3eae05e6a995ebfa6fd15fe829aafe94570626d
HF <3123961845@qq.com>
2025-07-30 21:59:53 +0800

    用户模块

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
-<script setup lang="ts">
-import BasicLayout from '@/layouts/BasicLayout.vue'
-</script>
-
 <template>
   <BasicLayout />
 </template>
 
+<script setup lang="ts">
+import BasicLayout from '@/layouts/BasicLayout.vue'
+import { useLoginUserStore } from '@/stores/loginUser.ts'
+
+const loginUserStore = useLoginUserStore()
+loginUserStore.fetchLoginUser()
+</script>
+
 <style>
+
 </style>
-
-
